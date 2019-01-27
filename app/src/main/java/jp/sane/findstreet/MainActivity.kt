64d6d823871f35f78af19ceeaff072c0ecb3139d
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-01-27 15:47:31 +0700

    chore(map): use mapbox

--- app/src/main/java/jp/sane/findstreet/MainActivity.kt
+++ app/src/main/java/jp/sane/findstreet/MainActivity.kt
@@ -2,11 +2,14 @@ package jp.sane.findstreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
     }
 }
